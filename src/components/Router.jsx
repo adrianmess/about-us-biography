681cf50d4fb3db806ce235854b29ccb347ff1f89
Ami <Guest@Epicodus-7C.local>
2018-06-28 08:40:47 -0700

    add homepage

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import 
-
+import BioPage from './BiographyPage';
+import Home from './Homepage';
 function Router(){
 
   return{
